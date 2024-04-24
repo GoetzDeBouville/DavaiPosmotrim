19aec34a3ff0c294457fdb942a154d19eb0b69d3
Dmitry Kryushnikov <civ02@mail.ru>
2024-04-24 23:50:18 +0500

    перенос MainActivity в пакет app

--- app/src/main/java/com/davay/android/app/MainActivity.kt
+++ app/src/main/java/com/davay/android/app/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.davay.android
+package com.davay.android.app
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.davay.android.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
