f07f0f29f19288a0d3dec1952887324969fbeee7
Dmitry Kryushnikov <civ02@mail.ru>
2024-04-26 22:43:21 +0500

    временная связь фрагмента и макета

--- app/src/main/java/com/davay/android/feature/load/presentation/LoadFragment.kt
+++ app/src/main/java/com/davay/android/feature/load/presentation/LoadFragment.kt
@@ -1,6 +1,18 @@
 package com.davay.android.feature.load.presentation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.davay.android.databinding.FragmentLoadBinding
 
 class LoadFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return FragmentLoadBinding.inflate(inflater, container, false).root
+    }
 }
