dd9fe32c5b683ebbab986b3b82e044ad66154059
Dmitry Kryushnikov <civ02@mail.ru>
2024-05-06 01:09:48 +0500

    примеры

--- uikit_sample/src/main/java/com/davai/uikit_sample/ButtonViewExampleActivity.kt
+++ uikit_sample/src/main/java/com/davai/uikit_sample/ButtonViewExampleActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.davai.uikit.PrimaryButtonView
+import com.davai.uikit.SecondaryButtonView
 
 class ButtonViewExampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,10 +14,20 @@ class ButtonViewExampleActivity : AppCompatActivity() {
 
         val button5 = findViewById<PrimaryButtonView>(R.id.button_5)
         val button6 = findViewById<PrimaryButtonView>(R.id.button_6)
+        val buttonSec2 = findViewById<SecondaryButtonView>(R.id.button_sec_2)
+        val buttonSec3 = findViewById<SecondaryButtonView>(R.id.button_sec_3)
+        val buttonSec4 = findViewById<SecondaryButtonView>(R.id.button_sec_4)
 
         button5.setButtonText("Какой-то текст")
         button5.setButtonEnabled(false)
         button6.setLoading(true)
         button6.setButtonText("Какой-то текст")
+
+        buttonSec2.setButtonText("Текст из кода")
+        buttonSec3.setButtonText("Текст из кода")
+        buttonSec3.setButtonEnabled(false)
+        buttonSec3.setButtonText("Текст из кода 2")
+        buttonSec4.setLoading(true)
+        buttonSec4.setButtonText("не должно быть видно")
     }
 }
