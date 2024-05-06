b7df750ff939cf9083b411fbf704e0c3d7f3f5a1
Dmitry Kryushnikov <civ02@mail.ru>
2024-05-06 23:18:10 +0500

    5_примеры слушателей на кнопке

--- uikit_sample/src/main/java/com/davai/uikit_sample/ButtonViewExampleActivity.kt
+++ uikit_sample/src/main/java/com/davai/uikit_sample/ButtonViewExampleActivity.kt
@@ -1,5 +1,6 @@
 package com.davai.uikit_sample
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +13,7 @@ class ButtonViewExampleActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_button_view_example)
 
+        val button1 = findViewById<PrimaryButtonView>(R.id.button_1)
         val button5 = findViewById<PrimaryButtonView>(R.id.button_5)
         val button6 = findViewById<PrimaryButtonView>(R.id.button_6)
         val buttonSec2 = findViewById<SecondaryButtonView>(R.id.button_sec_2)
@@ -29,5 +31,41 @@ class ButtonViewExampleActivity : AppCompatActivity() {
         buttonSec3.setButtonText("Текст из кода 2")
         buttonSec4.setLoading(true)
         buttonSec4.setButtonText("не должно быть видно")
+
+        button1.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@ButtonViewExampleActivity,
+                    MovieSelectionExampleActivity::class.java
+                )
+            )
+        }
+
+        button5.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@ButtonViewExampleActivity,
+                    MovieSelectionExampleActivity::class.java
+                )
+            )
+        }
+
+        button5.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@ButtonViewExampleActivity,
+                    MovieSelectionExampleActivity::class.java
+                )
+            )
+        }
+
+        buttonSec2.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@ButtonViewExampleActivity,
+                    MovieSelectionExampleActivity::class.java
+                )
+            )
+        }
     }
 }
