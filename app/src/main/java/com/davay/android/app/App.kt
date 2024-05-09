79b845dadf2e3a74609cb908460859c06a6a609e
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-10 01:01:15 +0300

    31_подключает коспонент AppComponent к application

--- app/src/main/java/com/davay/android/app/App.kt
+++ app/src/main/java/com/davay/android/app/App.kt
@@ -2,4 +2,9 @@ package com.davay.android.app
 
 import android.app.Application
 
-class App : Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppComponentHolder.createComponent(this)
+    }
+}
