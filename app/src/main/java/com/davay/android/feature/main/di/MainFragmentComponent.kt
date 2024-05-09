229c664b12385017be2fb388ed22559a4216f26e
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-10 01:12:13 +0300

    31_создает модуль и компонент для MainFragment провайдит зависимости для MainFragment

---
+++ app/src/main/java/com/davay/android/feature/main/di/MainFragmentComponent.kt
@@ -0,0 +1,18 @@
+package com.davay.android.feature.main.di
+
+import com.davay.android.app.AppComponent
+import com.davay.android.di.ScreenComponent
+import dagger.Component
+
+@Component(
+    dependencies = [AppComponent::class],
+    modules = [MainFragmentModule::class]
+)
+interface MainFragmentComponent : ScreenComponent {
+
+    @Component.Builder
+    interface Builder {
+        fun appComponent(appComponent: AppComponent): Builder
+        fun build(): MainFragmentComponent
+    }
+}
--- app/src/main/java/com/davay/android/feature/main/presentation/MainViewModel.kt
+++ app/src/main/java/com/davay/android/feature/main/presentation/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.davay.android.feature.main.presentation
 
 import com.davay.android.base.BaseViewModel
+import javax.inject.Inject
 
-class MainViewModel : BaseViewModel()
+class MainViewModel @Inject constructor() : BaseViewModel()
