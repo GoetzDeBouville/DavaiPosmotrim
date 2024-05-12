83145fa43800e54c76b4c7604dceab60ea7c479c
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-13 01:54:55 +0300

    28_добавляет инициализацию адаптера для viewpager

--- app/src/main/java/com/davay/android/feature/onboarding/fragment/OnboardingFragment.kt
+++ app/src/main/java/com/davay/android/feature/onboarding/fragment/OnboardingFragment.kt
@@ -1,34 +1,58 @@
 package com.davay.android.feature.onboarding.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.davay.android.app.AppComponentHolder
 import com.davay.android.base.BaseFragment
 import com.davay.android.databinding.FragmentOnboardingBinding
 import com.davay.android.di.ScreenComponent
-import com.davay.android.feature.load.di.DaggerLoadFragmentComponent
+import com.davay.android.feature.onboarding.adapter.OnboardingViewPagerAdapter
+import com.davay.android.feature.onboarding.di.DaggerOnBoardingFragmentComponent
 import com.davay.android.feature.onboarding.viewmodel.OnboardingViewModel
 
-class OnboardingFragment : BaseFragment<FragmentOnboardingBinding,
-        OnboardingViewModel>(FragmentOnboardingBinding::inflate) {
+class OnboardingFragment : BaseFragment<FragmentOnboardingBinding, OnboardingViewModel>(
+    FragmentOnboardingBinding::inflate
+) {
     override val viewModel: OnboardingViewModel by injectViewModel<OnboardingViewModel>()
 
-    override fun diComponent(): ScreenComponent = DaggerLoadFragmentComponent.builder()
+    override fun diComponent(): ScreenComponent = DaggerOnBoardingFragmentComponent.builder()
         .appComponent(AppComponentHolder.getComponent())
         .build()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
     }
 
     private fun initViews() = with(binding) {
-//        viewpager.adapter =
+        val fragmentList = listOf(
+            OnboardingFirstFragment(),
+            OnboardingSecondFragment(),
+            OnboardingThirdFragment()
+        )
+
+        viewpager.adapter = OnboardingViewPagerAdapter(
+            fragmentList,
+            viewLifecycleOwner.lifecycle,
+            this@OnboardingFragment.childFragmentManager
+        ) { position ->
+            updateButtonText(position)
+        }
+
+        ciIndicator.setViewPager(viewpager)
+    }
+
+    private fun updateButtonText(position: Int) = with(binding) {
+        mbtnFooterBtn.apply {
+            if (position == SECOND_FRAGMENT_POSITION) {
+                setText(com.davai.uikit.R.string.begin)
+            } else {
+                setText(com.davai.uikit.R.string.continue_view)
+            }
+        }
+    }
+
+    private companion object {
+        const val SECOND_FRAGMENT_POSITION = 2
     }
 }
