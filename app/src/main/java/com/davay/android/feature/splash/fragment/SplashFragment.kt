39f915e11789b754b303a9a19b0d2453687fce7d
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-13 17:26:33 +0300

    27_провайдит компонент и viewmodel во фрагмент

---
+++ app/src/main/java/com/davay/android/feature/splash/fragment/SplashFragment.kt
@@ -0,0 +1,17 @@
+package com.davay.android.feature.splash.fragment
+
+import com.davay.android.app.AppComponentHolder
+import com.davay.android.base.BaseFragment
+import com.davay.android.databinding.FragmentSplashBinding
+import com.davay.android.di.ScreenComponent
+import com.davay.android.feature.onboarding.di.DaggerOnBoardingFragmentComponent
+import com.davay.android.feature.splash.viewmodel.SplashViewModel
+
+class SplashFragment : BaseFragment<FragmentSplashBinding, SplashViewModel>(
+    FragmentSplashBinding::inflate
+) {
+    override val viewModel: SplashViewModel by injectViewModel<SplashViewModel>()
+    override fun diComponent(): ScreenComponent = DaggerOnBoardingFragmentComponent.builder()
+        .appComponent(AppComponentHolder.getComponent())
+        .build()
+}
