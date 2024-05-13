ec3581752255171e1f85d4f7fb229c1c94d115d1
Dmitry Kryushnikov <civ02@mail.ru>
2024-05-13 13:40:48 +0500

    32_принудительный вызов клавиатуры

--- app/src/main/java/com/davay/android/feature/registration/presentation/RegistrationFragment.kt
+++ app/src/main/java/com/davay/android/feature/registration/presentation/RegistrationFragment.kt
@@ -2,6 +2,8 @@ package com.davay.android.feature.registration.presentation
 
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import com.davay.android.base.BaseFragment
 import com.davay.android.databinding.FragmentRegistrationBinding
 
@@ -12,6 +14,13 @@ class RegistrationFragment() :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.etName.requestFocus()
+        showSoftKeyboard(binding.etName)
+    }
+
+    private fun showSoftKeyboard(view: View) {
+        if (view.requestFocus()) {
+            val imm = getSystemService(requireContext(), InputMethodManager::class.java)
+            imm?.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+        }
     }
 }
