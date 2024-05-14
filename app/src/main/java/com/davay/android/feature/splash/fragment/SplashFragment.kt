c91f91f86efb047e7afed97cdf6422325ea06fdf
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-14 12:37:28 +0300

    27_исправляет подключение компонента

--- app/src/main/java/com/davay/android/feature/splash/fragment/SplashFragment.kt
+++ app/src/main/java/com/davay/android/feature/splash/fragment/SplashFragment.kt
@@ -4,14 +4,14 @@ import com.davay.android.app.AppComponentHolder
 import com.davay.android.base.BaseFragment
 import com.davay.android.databinding.FragmentSplashBinding
 import com.davay.android.di.ScreenComponent
-import com.davay.android.feature.onboarding.di.DaggerOnBoardingFragmentComponent
+import com.davay.android.feature.splash.di.DaggerSplashFragmentComponent
 import com.davay.android.feature.splash.viewmodel.SplashViewModel
 
 class SplashFragment : BaseFragment<FragmentSplashBinding, SplashViewModel>(
     FragmentSplashBinding::inflate
 ) {
     override val viewModel: SplashViewModel by injectViewModel<SplashViewModel>()
-    override fun diComponent(): ScreenComponent = DaggerOnBoardingFragmentComponent.builder()
+    override fun diComponent(): ScreenComponent = DaggerSplashFragmentComponent.builder()
         .appComponent(AppComponentHolder.getComponent())
         .build()
 }
