a7c51ccc05e85200a9d9d1784c454b23f623d6c1
Dmitry Kryushnikov <civ02@mail.ru>
2024-05-15 19:08:10 +0500

    32_переделал отступы для клавиатуры

--- app/src/main/java/com/davay/android/app/MainActivity.kt
+++ app/src/main/java/com/davay/android/app/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.davay.android.app
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.davay.android.databinding.ActivityMainBinding
@@ -11,6 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            binding.root.setOnApplyWindowInsetsListener { v, insets ->
+                val imeHeight = insets.getInsets(WindowInsets.Type.ime()).bottom
+                binding.root.setPadding(0, 0, 0, imeHeight)
+                insets
+            }
+        }
 //        val navHostFragment =
 //            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
 //        val navController = navHostFragment.navController
