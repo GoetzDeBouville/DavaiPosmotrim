392838ac637bf94b6b48375c7cb8933894716747
Dmitry Kryushnikov <civ02@mail.ru>
2024-05-16 17:59:08 +0500

    32_правильное отображение отступов в версиях андроид меньше 30

--- app/src/main/java/com/davay/android/app/MainActivity.kt
+++ app/src/main/java/com/davay/android/app/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.davay.android.app
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.davay.android.databinding.ActivityMainBinding
 
@@ -12,18 +12,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(binding.root)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false)
-            binding.root.setOnApplyWindowInsetsListener { v, insets ->
-                val imeHeight = insets.getInsets(WindowInsetsCompat.Type.ime()).bottom
-                if (imeHeight != 0) {
-                    v.setPadding(0, 0, 0, imeHeight)
-                } else {
-                    v.setPadding(0, 0, 0, 0)
-                }
-                insets
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val imeHeight = insets.getInsets(WindowInsetsCompat.Type.ime()).bottom
+            if (insets.isVisible(WindowInsetsCompat.Type.ime())) {
+                v.setPadding(0, 0, 0, imeHeight)
+            } else {
+                v.setPadding(0, 0, 0, 0)
             }
+            insets
         }
 //        val navHostFragment =
 //            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
