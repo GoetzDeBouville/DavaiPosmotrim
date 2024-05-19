61df32711d10e781bff1974573b0db418b710999
HatePK <hatepk@gmail.com>
2024-05-20 00:51:58 +0300

    34_detekt

--- app/src/main/java/com/davay/android/feature/changename/presentation/ChangeNameFragment.kt
+++ app/src/main/java/com/davay/android/feature/changename/presentation/ChangeNameFragment.kt
@@ -110,7 +110,7 @@ class ChangeNameFragment(private val name: String) : BaseBottomSheetFragment<
 
     private fun buttonClicked() {
         viewModel.buttonClicked(binding.etName.text)
-        if (viewModel.state.value == ChangeNameState.SUCCESS){
+        if (viewModel.state.value == ChangeNameState.SUCCESS) {
             bottomSheetBehavior!!.state = BottomSheetBehavior.STATE_HIDDEN
         }
     }
