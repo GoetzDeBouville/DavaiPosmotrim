78b595e75f72bc7873ab41b3d29405a2aa82a7a9
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-20 14:28:04 +0300

    43_добавляет toolbar в разметку добавляет метод дефолтной настройки тулбара

--- app/src/main/java/com/davay/android/feature/selectmovie/presentation/SelectMovieFragment.kt
+++ app/src/main/java/com/davay/android/feature/selectmovie/presentation/SelectMovieFragment.kt
@@ -1,5 +1,8 @@
 package com.davay.android.feature.selectmovie.presentation
 
+import android.os.Bundle
+import android.view.View
+import com.davay.android.R
 import com.davay.android.app.AppComponentHolder
 import com.davay.android.base.BaseFragment
 import com.davay.android.databinding.FragmentSelectMovieBinding
@@ -10,8 +13,29 @@ class SelectMovieFragment :
     BaseFragment<FragmentSelectMovieBinding, SelectMovieViewModel>(FragmentSelectMovieBinding::inflate) {
 
     override val viewModel: SelectMovieViewModel by injectViewModel<SelectMovieViewModel>()
+    private var matchesCounter = 0 // TODO заменит на подписку
+
     override fun diComponent(): ScreenComponent = DaggerSelectMovieFragmentComponent
         .builder()
         .appComponent(AppComponentHolder.getComponent())
         .build()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        setDefaultToolbar()
+    }
+
+    private fun setDefaultToolbar() {
+        binding.toolbar.apply {
+            setStartIcon(com.davai.uikit.R.drawable.ic_cross)
+            setEndIcon(com.davai.uikit.R.drawable.ic_heart)
+            showEndIcon()
+            setTitleText(requireContext().getString(R.string.seelect_movies_select_film))
+            updateMatchesDisplay(matchesCounter)
+        }
+    }
 }
