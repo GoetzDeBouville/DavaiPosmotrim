f1cbe7d6f0d128866e090660f0b751f17fd697d3
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-20 12:21:25 +0300

    43_создает модуль и компонент, провайдит зависимости для фрагмента

--- app/src/main/java/com/davay/android/feature/selectmovie/presentation/SelectMovieFragment.kt
+++ app/src/main/java/com/davay/android/feature/selectmovie/presentation/SelectMovieFragment.kt
@@ -1,13 +1,17 @@
 package com.davay.android.feature.selectmovie.presentation
 
+import com.davay.android.app.AppComponentHolder
 import com.davay.android.base.BaseFragment
 import com.davay.android.databinding.FragmentSelectMovieBinding
 import com.davay.android.di.ScreenComponent
+import com.davay.android.feature.selectmovie.di.DaggerSelectMovieFragmentComponent
 
 class SelectMovieFragment :
     BaseFragment<FragmentSelectMovieBinding, SelectMovieViewModel>(FragmentSelectMovieBinding::inflate) {
+
     override val viewModel: SelectMovieViewModel by injectViewModel<SelectMovieViewModel>()
-    override fun diComponent(): ScreenComponent {
-        TODO("Not yet implemented")
-    }
+    override fun diComponent(): ScreenComponent = DaggerSelectMovieFragmentComponent
+        .builder()
+        .appComponent(AppComponentHolder.getComponent())
+        .build()
 }
