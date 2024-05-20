0608203a78d21b46503bc91f8e73d5c8ccb98f26
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-20 12:05:54 +0300

    43_добавляет пустой фрагмент

---
+++ app/src/main/java/com/davay/android/feature/selectmovie/presentation/SelectMovieFragment.kt
@@ -0,0 +1,11 @@
+package com.davay.android.feature.selectmovie.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.davay.android.R
+
+class SelectMovieFragment : Fragment() {
+}
