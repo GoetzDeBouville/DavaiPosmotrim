d7b6e4cfe7b5614a2def9c1972fe7ef225824db8
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-20 12:12:52 +0300

    43_провайдит viewmodel во фрагмент

--- app/src/main/java/com/davay/android/feature/selectmovie/presentation/SelectMovieFragment.kt
+++ app/src/main/java/com/davay/android/feature/selectmovie/presentation/SelectMovieFragment.kt
@@ -1,11 +1,13 @@
 package com.davay.android.feature.selectmovie.presentation
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.davay.android.R
+import com.davay.android.base.BaseFragment
+import com.davay.android.databinding.FragmentSelectMovieBinding
+import com.davay.android.di.ScreenComponent
 
-class SelectMovieFragment : Fragment() {
+class SelectMovieFragment :
+    BaseFragment<FragmentSelectMovieBinding, SelectMovieViewModel>(FragmentSelectMovieBinding::inflate) {
+    override val viewModel: SelectMovieViewModel by injectViewModel<SelectMovieViewModel>()
+    override fun diComponent(): ScreenComponent {
+        TODO("Not yet implemented")
+    }
 }
