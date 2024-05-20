35c7a688398a5cfe4426c1fbfe344b9ac754cfde
Eka666Sed <cat.sedoff@gmail.com>
2024-05-20 17:10:00 +0200

    40_добавляет верстку fragment_session_connection.xml

---
+++ app/src/main/java/com/davay/android/feature/sessionconnection/presentation/SessionConnectionFragment.kt
@@ -0,0 +1,8 @@
+package com.davay.android.feature.sessionconnection.presentation
+
+
+import androidx.fragment.app.Fragment
+
+class SessionConnectionFragment : Fragment() {
+
+}
---
+++ uikit/src/main/java/com/davai/uikit/ImeBackEditText.kt
@@ -0,0 +1,31 @@
+package com.davai.uikit
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.KeyEvent
+import androidx.appcompat.widget.AppCompatEditText
+
+/**
+Поле ввода текста, обрабатывающее нажатие кнопки назад.
+ */
+class ImeBackEditText : AppCompatEditText {
+
+    var buttonBackHandler: (() -> Unit)? = null
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    override fun onKeyPreIme(keyCode: Int, event: KeyEvent?): Boolean {
+        return if (keyCode == KeyEvent.KEYCODE_BACK && buttonBackHandler != null) {
+            buttonBackHandler!!.invoke()
+            false
+        } else {
+            super.onKeyPreIme(keyCode, event)
+        }
+    }
+}
