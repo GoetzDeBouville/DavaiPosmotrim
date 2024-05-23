28f8e7a4133d8f5a6fb6b9a883ff0a09862d55e8
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-23 20:38:16 +0300

    43_удаляет файлы bottomsheetfragment

--- app/src/main/java/com/davay/android/feature/moviedetails/presentation/MovieDetailsViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.davay.android.feature.moviedetails.presentation
-
-import com.davay.android.base.BaseViewModel
-import javax.inject.Inject
-
-class MovieDetailsViewModel @Inject constructor() : BaseViewModel()
