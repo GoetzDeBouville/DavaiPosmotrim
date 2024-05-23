c4d40f9cd37cffd3671efb442fd28383d343364a
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-23 16:35:25 +0300

    43_добавляет пустой фрагмент деталей фильма

---
+++ app/src/main/java/com/davay/android/feature/moviedetails/presentation/MovieDetailsBottomSheetFragment.kt
@@ -0,0 +1,5 @@
+package com.davay.android.feature.moviedetails.presentation
+
+import androidx.fragment.app.Fragment
+
+class MovieDetailsBottomSheetFragment : Fragment()
---
+++ app/src/main/java/com/davay/android/feature/moviedetails/presentation/MovieDetailsViewModel.kt
@@ -0,0 +1,6 @@
+package com.davay.android.feature.moviedetails.presentation
+
+import com.davay.android.base.BaseViewModel
+import javax.inject.Inject
+
+class MovieDetailsViewModel @Inject constructor() : BaseViewModel()
