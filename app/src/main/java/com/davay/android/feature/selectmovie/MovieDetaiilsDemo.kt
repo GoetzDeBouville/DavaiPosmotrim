78f7f07ef2e077dadf1dfe70c6295279d1d859af
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-24 15:27:44 +0300

    71_добавляет демо дата класс фильма

---
+++ app/src/main/java/com/davay/android/feature/selectmovie/MovieDetaiilsDemo.kt
@@ -0,0 +1,23 @@
+package com.davay.android.feature.selectmovie
+
+// используется для demo
+data class MovieDetaiilsDemo(
+    val kinopoiskId: Int? = null,
+    val imdbId: Int? = null,
+    val movieName: String,
+    val alternativeName: String? = null,
+    val englishName: String? = null,
+    val year: Int? = null,
+    val description: String? = null,
+    val shortDescription: String? = null,
+    val ratingKinopoisk: Float? = null,
+    val votesKinopoisk: Int? = null,
+    val ratingImdb: Float? = null,
+    val votesImdb: Int? = null,
+    val movieLengthMin: Int? = null,
+    val posterUrl: String? = null,
+    val genres: List<String> = emptyList(),
+    val countries: List<String> = emptyList(),
+    val topCast: List<String> = emptyList(),
+    val directors: List<String> = emptyList()
+)
