9808673f35df4b54a59811a29d916a2967bf0e78
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-25 23:40:47 +0300

    71_реализует методы onChildDraw и clearView в swipeCallback привязывает layoutManager для вызова свайп методов на событиях в onSwiped

--- app/src/main/java/com/davay/android/feature/selectmovie/adapters/SwipeCallback.kt
+++ app/src/main/java/com/davay/android/feature/selectmovie/adapters/SwipeCallback.kt
@@ -1,13 +1,23 @@
 package com.davay.android.feature.selectmovie.adapters
 
+import android.graphics.Canvas
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 
-class SwipeCallback(val onSwipedLeft: () -> Unit, val onSwipedRight: () -> Unit) :
-    ItemTouchHelper.SimpleCallback(
-        0,
-        ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
-    ) {
+class SwipeCallback(
+    private val layoutManager: SwipeableLayoutManager,
+    private val onSwipedLeft: () -> Unit,
+    private val onSwipedRight: () -> Unit
+) : ItemTouchHelper.Callback() {
+
+    override fun getMovementFlags(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder
+    ): Int {
+        val dragFlags = 0
+        val swipeFlags = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        return makeMovementFlags(dragFlags, swipeFlags)
+    }
 
     override fun onMove(
         recyclerView: RecyclerView,
@@ -19,8 +29,53 @@ class SwipeCallback(val onSwipedLeft: () -> Unit, val onSwipedRight: () -> Unit)
 
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
         when (direction) {
-            ItemTouchHelper.LEFT -> onSwipedLeft()
-            ItemTouchHelper.RIGHT -> onSwipedRight()
+            ItemTouchHelper.LEFT -> {
+                layoutManager.swipeLeft()
+                onSwipedLeft()
+            }
+
+            ItemTouchHelper.RIGHT -> {
+                layoutManager.swipeRight()
+                onSwipedRight()
+            }
         }
+    }
+
+    override fun onChildDraw(
+        canvas: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        viewHolder.itemView.apply {
+            rotation = dX / recyclerView.width * ROTATION_ANGLE_RANGE_45
+            translationX = dX
+            translationY = dY
+        }
+        super.onChildDraw(
+            canvas,
+            recyclerView,
+            viewHolder,
+            dX,
+            dY,
+            actionState,
+            isCurrentlyActive
+        )
+    }
+
+    override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+        super.clearView(recyclerView, viewHolder)
+        viewHolder.itemView.apply {
+            rotation = 0f
+            translationX = 0f
+            translationY = 0f
+        }
+    }
+
+    private companion object {
+        const val ROTATION_ANGLE_RANGE_45 = 45
     }
 }
