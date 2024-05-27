537c4e5859940d7d943a912930d24b90892baea1
HatePK <hatepk@gmail.com>
2024-05-27 11:27:24 +0300

    41 detekt

--- app/src/main/java/com/davay/android/feature/waitsession/presentation/adapter/CustomItemDecorator.kt
+++ app/src/main/java/com/davay/android/feature/waitsession/presentation/adapter/CustomItemDecorator.kt
@@ -4,7 +4,6 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-
 class CustomItemDecorator(private val spacing: Int) :
     RecyclerView.ItemDecoration() {
 
