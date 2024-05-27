ff1a2d2ca03ad7d0ab01c9681ca4d8ed8cfb5417
HatePK <hatepk@gmail.com>
2024-05-27 11:17:45 +0300

    41 добавлен кастомный Item Decorator в recyclerView и исправлен цвет текста на кнопке в темной теме

--- app/src/main/java/com/davay/android/feature/waitsession/presentation/WaitSessionFragment.kt
+++ app/src/main/java/com/davay/android/feature/waitsession/presentation/WaitSessionFragment.kt
@@ -5,6 +5,7 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
@@ -17,13 +18,14 @@ import com.davay.android.base.BaseFragment
 import com.davay.android.databinding.FragmentWaitSessionBinding
 import com.davay.android.di.ScreenComponent
 import com.davay.android.feature.waitsession.di.DaggerWaitSessionFragmentComponent
+import com.davay.android.feature.waitsession.presentation.adapter.CustomItemDecorator
 import com.davay.android.feature.waitsession.presentation.adapter.UserAdapter
 import com.google.android.flexbox.AlignItems
 import com.google.android.flexbox.FlexDirection
 import com.google.android.flexbox.FlexWrap
-import com.google.android.flexbox.FlexboxItemDecoration
 import com.google.android.flexbox.FlexboxLayoutManager
 import com.google.android.flexbox.JustifyContent
+import kotlin.math.roundToInt
 
 class WaitSessionFragment : BaseFragment<FragmentWaitSessionBinding, WaitSessionViewModel>(
     FragmentWaitSessionBinding::inflate
@@ -110,15 +112,21 @@ class WaitSessionFragment : BaseFragment<FragmentWaitSessionBinding, WaitSession
         }
 
         binding.rvUser.layoutManager = layoutManager
-
-        val itemDecoration = FlexboxItemDecoration(context)
-        val divider = ContextCompat.getDrawable(requireContext(), com.davai.uikit.R.drawable.recycler_view_divider)
-        itemDecoration.setDrawable(divider)
-
+        val spaceBetweenItems = convertPxToDp(requireContext(), SPACING_BETWEEN_RV_ITEMS)
+        val itemDecoration = CustomItemDecorator(spaceBetweenItems)
         binding.rvUser.addItemDecoration(itemDecoration)
     }
     override fun onDestroyView() {
         super.onDestroyView()
         userAdapter = null
+    }
+
+    private fun convertPxToDp(context: Context, px: Int): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return (px * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)).roundToInt()
+    }
+
+    companion object {
+        private const val SPACING_BETWEEN_RV_ITEMS = 8
     }
 }
