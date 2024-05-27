c5191a0cb1db46ae693bf8c0ff0fb55a2da201b8
Eka666Sed <cat.sedoff@gmail.com>
2024-05-27 16:09:05 +0200

    40_добавляет содержимое адаптера на session list

--- app/src/main/java/com/davay/android/feature/sessionlist/presentation/SessionListFragment.kt
+++ app/src/main/java/com/davay/android/feature/sessionlist/presentation/SessionListFragment.kt
@@ -1,14 +1,24 @@
 package com.davay.android.feature.sessionlist.presentation
 
 
+import android.content.Context
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.View
+import com.davay.android.R
 import com.davay.android.app.AppComponentHolder
 import com.davay.android.base.BaseFragment
 import com.davay.android.databinding.FragmentSessionListBinding
 import com.davay.android.di.ScreenComponent
 import com.davay.android.feature.sessionlist.di.DaggerSessionListFragmentComponent
+import com.davay.android.feature.sessionlist.presentation.adapter.CustomItemDecorator
 import com.davay.android.feature.sessionlist.presentation.adapter.UserAdapter
+import com.google.android.flexbox.AlignItems
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexWrap
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
+import kotlin.math.roundToInt
 
 class SessionListFragment : BaseFragment<FragmentSessionListBinding, SessionListViewModel>(
     FragmentSessionListBinding::inflate
@@ -23,13 +33,43 @@ class SessionListFragment : BaseFragment<FragmentSessionListBinding, SessionList
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val currentToolbarText = binding.toolbar.setTitleText("Сеанс" + " " + "VMst457")
+        binding.toolbar.setTitleText(getString(R.string.session_list_name) + " " + "VMst457")
 
-//        initRecycler()
-//        userAdapter?.itemList?.addAll(
-//            listOf("Артем", "Руслан", "Константин", "Виктория")
-//        )
-
+        initRecycler()
+        userAdapter?.itemList?.addAll(
+            listOf("Артем", "Руслан", "Константин", "Виктория")
+        )
     }
 
+    private fun initRecycler() {
+        userAdapter = UserAdapter()
+        binding.rvUser.adapter = userAdapter
+        val layoutManager = FlexboxLayoutManager(context).apply {
+            flexDirection = FlexDirection.ROW
+            flexWrap = FlexWrap.WRAP
+            justifyContent = JustifyContent.FLEX_START
+            alignItems = AlignItems.FLEX_START
+        }
+
+        binding.rvUser.layoutManager = layoutManager
+        val spaceBetweenItems = convertPxToDp(requireContext(), SPACING_BETWEEN_RV_ITEMS)
+        val itemDecoration = CustomItemDecorator(spaceBetweenItems)
+        binding.rvUser.addItemDecoration(itemDecoration)
+    }
+
+    override fun onDestroyView() {
+        binding.rvUser.adapter = null
+        super.onDestroyView()
+        userAdapter = null
+    }
+
+    private fun convertPxToDp(context: Context, px: Int): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return (px * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)).roundToInt()
+    }
+
+    companion object {
+        private const val SPACING_BETWEEN_RV_ITEMS = 8
+    }
 }
+
