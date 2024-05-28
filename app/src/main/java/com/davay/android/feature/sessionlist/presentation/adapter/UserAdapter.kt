ce3bdef91ca4ed20784e70eadfc884be4a13e3b8
Eka666Sed <cat.sedoff@gmail.com>
2024-05-28 15:53:22 +0200

    40_редактирует adapter и UserViewHolder, удаляет использование tag_view и добавляет использование tag_item для участников, выносит строковый ресурс из кода

--- app/src/main/java/com/davay/android/feature/sessionlist/presentation/adapter/UserAdapter.kt
+++ app/src/main/java/com/davay/android/feature/sessionlist/presentation/adapter/UserAdapter.kt
@@ -3,21 +3,23 @@ package com.davay.android.feature.sessionlist.presentation.adapter
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.davai.uikit.TagView
+import com.davay.android.R
 
 class UserAdapter :
     RecyclerView.Adapter<UserViewHolder>() {
     val itemList: MutableList<String> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        val tagView = TagView(parent.context)
-        return UserViewHolder(tagView)
+        return UserViewHolder.from(parent)
     }
 
     override fun getItemCount(): Int = itemList.count()
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val context = holder.itemView.context
+        val youSuffix = context.getString(R.string.session_list_you)
         if (position == 0) {
-            holder.bind("${itemList[position]} (вы)")
+            holder.bind("${itemList[position]} $youSuffix")
         } else {
             holder.bind(itemList[position])
         }
--- app/src/main/java/com/davay/android/feature/sessionlist/presentation/adapter/UserViewHolder.kt
+++ app/src/main/java/com/davay/android/feature/sessionlist/presentation/adapter/UserViewHolder.kt
@@ -1,13 +1,23 @@
 package com.davay.android.feature.sessionlist.presentation.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.davai.uikit.TagView
+import com.davay.android.R
 
 class UserViewHolder(
     private val tag: TagView
 ) : RecyclerView.ViewHolder(tag) {
     fun bind(user: String) {
-        tag.changeStyle(TagView.Companion.Style.SECONDARY_GREEN)
         tag.setText(user)
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): UserViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = layoutInflater.inflate(com.davai.uikit.R.layout.tag_item, parent, false) as TagView
+            return UserViewHolder(view)
+        }
     }
 }
