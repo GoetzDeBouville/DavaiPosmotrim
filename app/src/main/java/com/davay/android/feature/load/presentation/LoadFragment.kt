af3fa7cbca613677063e3d24f177a90e275fe2f3
Dmitry Kryushnikov <civ02@mail.ru>
2024-06-02 01:34:38 +0500

    61_переход на рулетку с первого экрана

--- app/src/main/java/com/davay/android/feature/load/presentation/LoadFragment.kt
+++ app/src/main/java/com/davay/android/feature/load/presentation/LoadFragment.kt
@@ -40,5 +40,8 @@ class LoadFragment : BaseFragment<FragmentLoadBinding, LoadViewModel>(
         binding.button2.setOnClickListener { _ ->
             viewModel.navigate(R.id.action_loadFragment_to_registrationFragment)
         }
+        binding.btnToRoulette.setOnClickListener {
+            viewModel.navigate(R.id.action_loadFragment_to_rouletteFragment)
+        }
     }
 }
