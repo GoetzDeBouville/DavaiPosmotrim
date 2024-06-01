246e5b6b8ff3c944289859c6fe2b9affcea45a8a
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-06-01 18:19:00 +0300

    72_добавляет обработчик нажатий в кастомную view (по клику производит замену на secondary изображение)

--- uikit/src/main/java/com/davai/uikit/ConditionedImageView.kt
+++ uikit/src/main/java/com/davai/uikit/ConditionedImageView.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.RectF
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import androidx.annotation.AttrRes
 import androidx.annotation.StyleRes
@@ -17,7 +18,7 @@ class ConditionedImageView @JvmOverloads constructor(
     @AttrRes defStyleAttr: Int = 0,
     @StyleRes defStyleRes: Int = 0
 ) : View(context, attrs, defStyleAttr, defStyleAttr) {
-    private val minViewSize = resources.getDimensionPixelSize(R.dimen.default_icon_size_40)
+    private val minViewSize = resources.getDimensionPixelSize(R.dimen.default_icon_size_52)
     private var imageRect = RectF(0f, 0f, 0f, 0f)
     private var imageBitmap: Bitmap? = null
     private var baseConditionBitmap: Bitmap? = null
@@ -28,7 +29,8 @@ class ConditionedImageView @JvmOverloads constructor(
             context,
             attrs,
             defStyleAttr,
-            defStyleRes)
+            defStyleRes
+        )
     }
 
     private fun applyAttributes(
@@ -48,12 +50,12 @@ class ConditionedImageView @JvmOverloads constructor(
                     getDrawable(R.styleable.ConditionedImageView_baseConditionImageId)?.toBitmap()
                 secondConditionBitmap =
                     getDrawable(R.styleable.ConditionedImageView_secondConditionImageId)?.toBitmap()
+                imageBitmap = baseConditionBitmap
             } finally {
                 recycle()
             }
         }
     }
-
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
@@ -74,5 +76,23 @@ class ConditionedImageView @JvmOverloads constructor(
         imageBitmap?.let {
             canvas.drawBitmap(it, null, imageRect, null)
         }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                return true
+            }
+
+            MotionEvent.ACTION_UP -> {
+                if (isClickable) {
+                    imageBitmap = secondConditionBitmap
+                    invalidate()
+                    performClick()
+                    return true
+                }
+            }
+        }
+        return super.onTouchEvent(event)
     }
 }
