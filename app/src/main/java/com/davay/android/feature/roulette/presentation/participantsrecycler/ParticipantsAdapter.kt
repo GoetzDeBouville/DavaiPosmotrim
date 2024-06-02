e37e5eb69c7e7f1dd081e26493654f157caf69ca
Dmitry Kryushnikov <civ02@mail.ru>
2024-06-02 22:21:47 +0500

    61_пустые адаптер и viewHolder

---
+++ app/src/main/java/com/davay/android/feature/roulette/presentation/participantsrecycler/ParticipantsAdapter.kt
@@ -0,0 +1,18 @@
+package com.davay.android.feature.roulette.presentation.participantsrecycler
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+
+class ParticipantsAdapter : Adapter<ParticipantsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ParticipantsViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ParticipantsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/davay/android/feature/roulette/presentation/participantsrecycler/ParticipantsViewHolder.kt
@@ -0,0 +1,7 @@
+package com.davay.android.feature.roulette.presentation.participantsrecycler
+
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.davay.android.databinding.ItemParticipantsBinding
+
+class ParticipantsViewHolder(private val binding: ItemParticipantsBinding) :
+    ViewHolder(binding.root)
