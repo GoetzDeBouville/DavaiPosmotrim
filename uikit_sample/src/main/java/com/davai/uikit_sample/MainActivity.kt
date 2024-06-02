59a6d3ebf5c93ad16b2693cf4ab984fca6657cd9
Eka666Sed <cat.sedoff@gmail.com>
2024-06-02 18:07:37 +0200

    83_добавляет поведение dialog

--- uikit_sample/src/main/java/com/davai/uikit_sample/MainActivity.kt
+++ uikit_sample/src/main/java/com/davai/uikit_sample/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.davai.uikit_sample
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.davai.uikit_sample.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -26,7 +33,8 @@ class MainActivity : AppCompatActivity() {
             btnToDvMovieSelection,
             btnToDvToolbar,
             btnToDvTags,
-            toMsb
+            toMsb,
+            btnToCustomDialog
         ).forEach {
             it.setOnClickListener(onClickListener())
         }
@@ -82,7 +90,24 @@ class MainActivity : AppCompatActivity() {
                         TagViewExample::class.java
                     )
                 )
+                
+                btnToCustomDialog -> {val message : String? = "Are you sure?"
+                    showCustomDialogBox(message)}
             }
         }
+    }
+
+    private fun showCustomDialogBox(message: String?) {
+    val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(com.davai.uikit.R.layout.layout_custom_dialog)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val tvTitle: TextView = dialog.findViewById(com.davai.uikit.R.id.tv_dialog_title)
+        val tvMessage: TextView = dialog.findViewById(com.davai.uikit.R.id.tv_dialog_message)
+        val btnYes: Button = dialog.findViewById(com.davai.uikit.R.id.btn_yes)
+        val btnNo: Button = dialog.findViewById(com.davai.uikit.R.id.btn_no)
+        tvMessage.text = message
     }
 }
