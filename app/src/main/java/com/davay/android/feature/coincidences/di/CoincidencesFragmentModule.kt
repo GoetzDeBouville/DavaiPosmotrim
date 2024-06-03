8afa5aa08fac2c21ec0d2e4a1f646e2bb6d9a50e
megrief <eu.pim@yandex.ru>
2024-06-03 10:21:52 +0500

    Добавлены: - Классы di: CoincidencesFragmentComponent и CoincidencesFragmentModule - ViewModel и Fragment для экрана совпадений.

---
+++ app/src/main/java/com/davay/android/feature/coincidences/di/CoincidencesFragmentModule.kt
@@ -0,0 +1,17 @@
+package com.davay.android.feature.coincidences.di
+
+import androidx.lifecycle.ViewModel
+import com.davay.android.di.ViewModelKey
+import com.davay.android.feature.coincidences.presentation.CoincidencesViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+interface CoincidencesFragmentModule {
+
+    @IntoMap
+    @ViewModelKey(CoincidencesViewModel::class)
+    @Binds
+    fun bindVM(impl: CoincidencesViewModel): ViewModel
+}
---
+++ app/src/main/java/com/davay/android/feature/coincidences/presentation/CoincidencesViewModel.kt
@@ -0,0 +1,7 @@
+package com.davay.android.feature.coincidences.presentation
+
+import com.davay.android.base.BaseViewModel
+import javax.inject.Inject
+
+class CoincidencesViewModel @Inject constructor() : BaseViewModel() {
+}
