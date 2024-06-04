77809210ebc6e7006a3840fc5af6526732198726
Eka666Sed <cat.sedoff@gmail.com>
2024-06-04 19:15:15 +0200

    83_добавляет blurEffect

--- uikit_sample/src/main/java/com/davai/uikit_sample/CustomDialog.kt
+++ uikit_sample/src/main/java/com/davai/uikit_sample/CustomDialog.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.DialogFragment
+import com.davai.uikit.extensions.applyBlurEffect
+import com.davai.uikit.extensions.clearBlurEffect
 
 class CustomDialog : DialogFragment() {
 
@@ -14,7 +17,22 @@ class CustomDialog : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         dialog?.window?.setBackgroundDrawableResource(com.davai.uikit.R.drawable.session_card_background)
-        return inflater.inflate(com.davai.uikit.R.layout.layout_custom_dialog, container, false)
+        val view = inflater.inflate(com.davai.uikit.R.layout.layout_custom_dialog, container, false)
+        activity?.window?.decorView?.applyBlurEffect()
+        val btnYes = view.findViewById<Button>(com.davai.uikit.R.id.btn_yes)
+        val btnNo = view.findViewById<Button>(com.davai.uikit.R.id.btn_no)
+
+        btnYes.setOnClickListener {
+            dialog?.dismiss()
+            activity?.window?.decorView?.clearBlurEffect()
+        }
+
+        btnNo.setOnClickListener {
+            dialog?.dismiss()
+            activity?.window?.decorView?.clearBlurEffect()
+        }
+
+        return view
     }
 
     override fun onStart() {
