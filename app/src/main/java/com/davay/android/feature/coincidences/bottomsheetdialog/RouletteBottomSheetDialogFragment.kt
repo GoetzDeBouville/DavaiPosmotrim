16f756290800399c07d143d86f762d5779e4766c
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-06-21 01:34:10 +0300

    89_добавляет диалог фрагмент

---
+++ app/src/main/java/com/davay/android/feature/coincidences/bottomsheetdialog/RouletteBottomSheetDialogFragment.kt
@@ -0,0 +1,29 @@
+package com.davay.android.feature.coincidences.bottomsheetdialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.davay.android.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.button.MaterialButton
+
+class RouletteBottomSheetDialogFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.fragment_roulette_bottom_sheet_dialog,
+            container,
+            false
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<MaterialButton>(R.id.mbtn_roulette_dialog_footer_btn).setOnClickListener {
+            dismiss()
+        }
+    }
+}
