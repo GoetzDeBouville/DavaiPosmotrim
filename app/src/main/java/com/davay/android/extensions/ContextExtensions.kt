5148885d5f00c04fd9803c8d2c0f5e628377bd0e
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-06-26 01:12:38 +0300

    103_добавляет расширение на контекст для проверки подключения к сети

---
+++ app/src/main/java/com/davay/android/extensions/ContextExtensions.kt
@@ -0,0 +1,22 @@
+package com.davay.android.extensions
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+
+fun Context.isInternetReachable(): Boolean {
+    val connectivityManager =
+        this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val activeNetwork = connectivityManager.activeNetwork?.let {
+        connectivityManager.getNetworkCapabilities(it)
+    }
+
+    return activeNetwork?.let {
+        val hasWifiTransport = it.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+        val hasCellularTransport = it.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+        val hasEthernetTransport = it.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+
+        hasWifiTransport || hasCellularTransport || hasEthernetTransport
+    } ?: false
+}
