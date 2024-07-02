60c584c0ef8eb49c425ec8614d9494b4c699a39c
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-07-02 03:35:19 +0300

    62_добавляет в адаптер форматирование строки списка юзеров и даты

--- app/src/main/java/com/davay/android/feature/sessionsmatched/presentation/adapters/SessionListAdapter.kt
+++ app/src/main/java/com/davay/android/feature/sessionsmatched/presentation/adapters/SessionListAdapter.kt
@@ -1,10 +1,12 @@
 package com.davay.android.feature.sessionsmatched.presentation.adapters
 
+import android.icu.text.SimpleDateFormat
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.davay.android.databinding.ItemSessionBinding
 import com.davay.android.domain.models.Session
+import java.util.Locale
 
 class SessionListAdapter(
     private val onSessionClickListener: (id: String) -> Unit?
@@ -17,14 +19,23 @@ class SessionListAdapter(
         fun bind(session: Session) {
             val userList = session.users.toMutableList()
             userList[0] = userList[0].copy(name = userList[0].name + " (Вы)")
+            val userNames = userList.map { it.name }
+            val formatedDate = formatDate(session.date)
             binding.root.apply {
-                setDate(session.date)
+                setDate(formatedDate)
                 setNamesList(
-                    userList.joinToString(" ,")
+                    userNames.joinToString(", ")
                 )
                 setCover(session.imgUrl)
                 setCoincidences(session.numberOfMatchedMovies ?: 0)
             }
+        }
+
+        private fun formatDate(inputDate: String): String {
+            val inputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            val outputFormat = SimpleDateFormat("dd MMMM yyyy", Locale("ru"))
+            val date = inputFormat.parse(inputDate)
+            return outputFormat.format(date)
         }
     }
 
