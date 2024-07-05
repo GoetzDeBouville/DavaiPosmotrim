0614b8089696b03a413706b94e4c507b0590415f
Dmitry Kryushnikov <civ02@mail.ru>
2024-07-06 00:32:43 +0500

    116_DAO

--- app/src/main/java/com/davay/android/data/database/AppDatabase.kt
+++ app/src/main/java/com/davay/android/data/database/AppDatabase.kt
@@ -2,10 +2,16 @@ package com.davay.android.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.davay.android.data.database.entity.MovieDetailsEntity
 import com.davay.android.data.database.entity.SessionEntity
+import com.davay.android.data.database.entity.SessionMovieCrossRef
 
-@Database(entities = [SessionEntity::class, MovieDetailsEntity::class], version = 1)
+@Database(
+    entities = [SessionEntity::class, MovieDetailsEntity::class, SessionMovieCrossRef::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
-
+    abstract fun historyDao(): HistoryDao
 }
---
+++ app/src/main/java/com/davay/android/data/database/HistoryDao.kt
@@ -0,0 +1,31 @@
+package com.davay.android.data.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import com.davay.android.data.database.entity.MovieDetailsEntity
+import com.davay.android.data.database.entity.SessionEntity
+import com.davay.android.data.database.entity.SessionMovieCrossRef
+import com.davay.android.data.database.entity.SessionWithMovies
+
+@Dao
+interface HistoryDao {
+
+    @Query("SELECT * FROM sessions")
+    suspend fun getSessions(): List<SessionEntity>
+
+    @Transaction
+    @Query("SELECT * FROM sessions WHERE sessionId = :sessionId")
+    suspend fun getSessionWithMovies(sessionId: Long): SessionWithMovies
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSession(session: SessionEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovie(movie: MovieDetailsEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSessionWithMovie(sessionWithMovie: SessionMovieCrossRef)
+}
