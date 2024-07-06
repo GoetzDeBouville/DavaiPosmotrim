16e013106a184f27b4d1719dcfecf039b0d6611c
Dmitry Kryushnikov <civ02@mail.ru>
2024-07-06 16:15:57 +0500

    116_комментарии и переименование

--- app/src/main/java/com/davay/android/data/database/HistoryDao.kt
+++ app/src/main/java/com/davay/android/data/database/HistoryDao.kt
@@ -13,19 +13,24 @@ import com.davay.android.data.database.entity.SessionWithMovies
 @Dao
 interface HistoryDao {
 
+    // Получение всех сохраненных сессий
     @Query("SELECT * FROM sessions")
     suspend fun getSessions(): List<SessionEntity>
 
+    // Получение по sessionId сессии с её списком фильмов. Если такой сессии нет, то null
     @Transaction
     @Query("SELECT * FROM sessions WHERE sessionId = :sessionId")
     suspend fun getSessionWithMovies(sessionId: String): SessionWithMovies?
 
+    // Сохранение сессии (без фильмов)
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertSession(session: SessionEntity)
 
+    // Сохранение фильма
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMovie(movie: MovieDetailsEntity)
 
+    // Сохранение связи сессии и фильма с помощью sessionId и movieId
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertSessionWithMovie(sessionWithMovie: SessionMovieCrossRef)
+    suspend fun insertSessionMovieReference(sessionMovieCrossRef: SessionMovieCrossRef)
 }
