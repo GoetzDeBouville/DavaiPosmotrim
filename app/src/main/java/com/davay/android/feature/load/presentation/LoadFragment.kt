b46523b1fb39ba98d83ec4cbef75eb40cd240173
muskilon <max-post@yandex.ru>
2024-07-07 11:18:53 +0300

    63_переделал пример с использованием модели для сессии

--- app/src/main/java/com/davay/android/feature/load/presentation/LoadFragment.kt
+++ app/src/main/java/com/davay/android/feature/load/presentation/LoadFragment.kt
@@ -9,6 +9,9 @@ import com.davay.android.app.AppComponentHolder
 import com.davay.android.base.BaseFragment
 import com.davay.android.databinding.FragmentLoadBinding
 import com.davay.android.di.ScreenComponent
+import com.davay.android.domain.models.Session
+import com.davay.android.domain.models.SessionStatus
+import com.davay.android.domain.models.User
 import com.davay.android.feature.load.di.DaggerLoadFragmentComponent
 import com.davay.android.feature.matchedsession.presentation.MatchedSessionFragment
 import com.davay.android.feature.onboarding.presentation.OnboardingFragment
@@ -75,21 +78,48 @@ class LoadFragment : BaseFragment<FragmentLoadBinding, LoadViewModel>(
             viewModel.navigate(R.id.action_loadFragment_to_waitSessionFragment)
         }
         binding.btnToMatchedSession.setOnClickListener {
-            viewModel.navigate(R.id.action_loadFragment_to_matchedSessionFragment, getBundleForMatchedSession())
+            navigateToMatchedSession()
         }
     }
 
-    private fun getBundleForMatchedSession(): Bundle =
-//        Данные берем из модели, переход будет со страницы списка сессий, там все это будет
-        Bundle().apply {
-            putString(MatchedSessionFragment.SUBTITLE, "${getString(R.string.session_list_name)} VMst456")
-            putStringArray(MatchedSessionFragment.USERS, arrayOf("Артём", "Виктория", "Иван", "Марина", "Фёдор"))
-            val simpleDate =
-                SimpleDateFormat(
-                    "yyyy-MM-dd",
-                    Locale.getDefault()
-                ).parse("2023-09-23") ?: Calendar.getInstance().time
-            val formater = SimpleDateFormat("dd MMMM", Locale.getDefault())
-            putString(MatchedSessionFragment.DATE, formater.format(simpleDate))
-        }
+    private fun navigateToMatchedSession() {
+        val bundle =
+            Bundle().apply {
+                putString(
+                    MatchedSessionFragment.SUBTITLE,
+                    "${getString(R.string.session_list_name)} ${session.id}"
+                )
+                val users = session.users.map { it.name }.toTypedArray()
+                putStringArray(MatchedSessionFragment.USERS, users)
+                val simpleDate =
+                    SimpleDateFormat(
+                        "yyyy-MM-dd",
+                        Locale.getDefault()
+                    ).parse(session.date) ?: Calendar.getInstance().time
+                val formater = SimpleDateFormat("dd MMMM", Locale.getDefault())
+                putString(MatchedSessionFragment.DATE, formater.format(simpleDate))
+            }
+        viewModel.navigate(
+            R.id.action_loadFragment_to_matchedSessionFragment,
+            bundle
+        )
+    }
+
+    companion object {
+        //        Данные будем брать из модели, переход будет со страницы списка сессий, там все это будет
+        val session = Session(
+            id = "VMst456",
+            date = "2023-09-23",
+            imgUrl = "//imgUrl",
+            users = listOf(
+                User("1", "Артём"),
+                User("2", "Виктория"),
+                User("3", "Иван"),
+                User("4", "Марина"),
+                User("5", "Фёдор")
+            ),
+            status = SessionStatus.CLOSED,
+            numberOfMatchedMovies = 2
+        )
+    }
 }
