745841fd9ec6effbf7f2dfd3b656fdd5b897bb5f
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-07-21 00:34:53 +0300

    94_добавляет методы навигации в базовую viewmodel добавляет обработчик навигации с navoptions

--- app/src/main/java/com/davay/android/base/BaseViewModel.kt
+++ app/src/main/java/com/davay/android/base/BaseViewModel.kt
@@ -5,6 +5,8 @@ import androidx.annotation.IdRes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavOptions
+import com.davay.android.R
 
 abstract class BaseViewModel : ViewModel() {
 
@@ -19,8 +21,31 @@ abstract class BaseViewModel : ViewModel() {
         _navigation.value = Event(NavigationCommand.ToDirection(navDirections, bundle))
     }
 
+    fun navigate(@IdRes navDirections: Int, navOptions: NavOptions) {
+        _navigation.value = Event(NavigationCommand.ToDirection(navDirections, null, navOptions))
+    }
+
+    fun navigate(@IdRes navDirections: Int, bundle: Bundle, navOptions: NavOptions) {
+        _navigation.value = Event(NavigationCommand.ToDirection(navDirections, bundle, navOptions))
+    }
+
     fun navigateBack() {
         _navigation.value = Event(NavigationCommand.Back)
     }
 
+    /**
+     * Метод чистит backstack до MainFragment и делает сооветствующий переход
+     */
+    fun clearBackStackToMain() {
+        val navOptions = NavOptions.Builder()
+            .setPopUpTo(R.id.main_navigation_graph, inclusive = true)
+            .build()
+
+        navigate(R.id.mainFragment, navOptions)
+    }
+
+    fun clearBackStackToMainAndNavigate(@IdRes navDirections: Int) {
+        clearBackStackToMain()
+        navigate(navDirections)
+    }
 }
