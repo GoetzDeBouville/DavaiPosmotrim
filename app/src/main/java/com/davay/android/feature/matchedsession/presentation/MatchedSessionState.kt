5f9fe2f4b4be790277cc992d89944588d7a811c8
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-07-20 13:37:43 +0300

    125_создает модель состояний для экрана сессии из истории

---
+++ app/src/main/java/com/davay/android/feature/matchedsession/presentation/MatchedSessionState.kt
@@ -0,0 +1,12 @@
+package com.davay.android.feature.matchedsession.presentation
+
+import com.davay.android.domain.models.ErrorType
+import com.davay.android.feature.matchedsession.domain.SessionWithMovies
+
+sealed class MatchedSessionState {
+
+    data object Empty : MatchedSessionState()
+    data object Loading : MatchedSessionState()
+    class Data(val data: SessionWithMovies) : MatchedSessionState()
+    class Error(val errorType: ErrorType) : MatchedSessionState()
+}
