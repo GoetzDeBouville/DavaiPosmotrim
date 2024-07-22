43828116e0fa88ecfe7f9a909d41ebe24c6e971d
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-07-22 17:21:36 +0300

    133_обновляет список ошибок

--- app/src/main/java/com/davay/android/data/network/ApiConstants.kt
+++ app/src/main/java/com/davay/android/data/network/ApiConstants.kt
@@ -1,17 +1,12 @@
 package com.davay.android.data.network
 
 object ApiConstants {
-    const val BASE_URL = "http://80.87.108.90/"
+    const val BASE_URL = "http://80.87.108.90/api/"
 
-    const val SUCCESS_CODE = 200
     const val NO_INTERNET_CONNECTION_CODE = -1
-    const val BAD_REQUEST_CODE = 400
+    const val SUCCESS_CODE = 200
+    const val SUCCESSFULLY_ADDED_CODE = 201
+    const val SUCCESSFULLY_REMOVED_CODE = 204
     const val BAD_REQUEST = 400
-    const val UNAUTHORIZED = 401
-    const val FORBIDDEN = 403
     const val NOT_FOUND = 404
-    const val CONFLICT = 409
-    const val UNPROCESSABLE_ENTITY = 422
-    const val INTERNAL_SERVER_ERROR = 500
-    const val BAD_GATEWAY = 502
 }
