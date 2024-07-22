5a9cb4ffa9d92637cf2100ee442957aadbacc82b
Mike Pavlov <7links91@mail.ru>
2024-07-23 00:15:26 +0300

    Правочки для бота

--- uikit/src/main/java/com/davai/uikit/MainDialogViewModel.kt
+++ uikit/src/main/java/com/davai/uikit/MainDialogViewModel.kt
@@ -2,7 +2,7 @@ package com.davai.uikit
 
 import androidx.lifecycle.ViewModel
 
-class MainDialogViewModel: ViewModel() {
+class MainDialogViewModel : ViewModel() {
     var title: String? = null
     var message: String? = null
     var yesAction: (() -> Unit)? = null
