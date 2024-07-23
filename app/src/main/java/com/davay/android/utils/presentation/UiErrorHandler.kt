04edafd4c57bc17866bd7af6e814ccb9c53dd2f8
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-07-23 17:48:16 +0300

    133_добавляет лямбду в обработчик ошибок

--- app/src/main/java/com/davay/android/utils/presentation/UiErrorHandler.kt
+++ app/src/main/java/com/davay/android/utils/presentation/UiErrorHandler.kt
@@ -1,9 +1,22 @@
 package com.davay.android.utils.presentation
 
-import android.view.View
 import com.davai.uikit.ErrorScreenView
 import com.davay.android.domain.models.ErrorScreenState
 
+/**
+ * UiErrorHandler необходим для обработки отображения ошибок, чтобы не дублировать код в каждом фрагменте.
+ * UiErrorHandler работает только с конкретным типом View - ErrorScreenView.
+ */
 interface UiErrorHandler {
-    fun handleError(errorScreenState: ErrorScreenState, errorLayout: ErrorScreenView)
+    /**
+     * Метод приминает унифицированный стэйт ошибки ErrorScreenState, кастомную View ErrorScreenView
+     * и в action передается лямбда для слушателя кликов на кнопке в стэйте NO_INTERNET. В стэйте
+     * APP_VERSION_ERROR будет отработан переход на страницу приложения в PlayMarket. В остальных
+     * стэйтах кнопка не отображается.
+     */
+    fun handleError(
+        errorScreenState: ErrorScreenState,
+        errorLayout: ErrorScreenView,
+        action: (() -> Unit)?
+    )
 }
--- app/src/main/java/com/davay/android/utils/presentation/UiErrorHandlerImpl.kt
+++ app/src/main/java/com/davay/android/utils/presentation/UiErrorHandlerImpl.kt
@@ -1,10 +1,17 @@
 package com.davay.android.utils.presentation
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import com.davai.uikit.ErrorScreenView
 import com.davay.android.domain.models.ErrorScreenState
 
 class UiErrorHandlerImpl : UiErrorHandler {
-    override fun handleError(errorScreenState: ErrorScreenState, errorLayout: ErrorScreenView) {
+    override fun handleError(
+        errorScreenState: ErrorScreenState,
+        errorLayout: ErrorScreenView,
+        action: (() -> Unit)?
+    ) {
         errorLayout.apply {
             val title = this.context.resources.getString(errorScreenState.errorTitleResource)
             val description =
@@ -19,6 +26,37 @@ class UiErrorHandlerImpl : UiErrorHandler {
             setErrorTitle(title)
             setErrorDescription(description)
             setButtonText(buttonText)
+
+            if (action == null) {
+                openAppInPlayStore(errorLayout.context)
+            } else {
+                action.invoke()
+            }
         }
+    }
+
+    /**
+     * Переход на страницу приложения в PlayMarket
+     * !Уточнить App id
+     */
+    private fun openAppInPlayStore(context: Context) {
+        try {
+            context.startActivity(
+                Intent(Intent.ACTION_VIEW, Uri.parse(WEB_ADDRESS + APP_ID))
+            )
+        } catch (e: Exception) {
+            context.startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(WEB_ADDRESS_INTENT + APP_ID)
+                )
+            )
+        }
+    }
+
+    private companion object {
+        const val APP_ID = "com.davay.DavayPosmotrim"
+        const val WEB_ADDRESS = "https://play.google.com/store/apps/details?id="
+        const val WEB_ADDRESS_INTENT = "market://details?id="
     }
 }
