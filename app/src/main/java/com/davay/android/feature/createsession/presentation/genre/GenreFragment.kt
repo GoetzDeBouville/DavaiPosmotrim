86a1804125f09674bbeb257ae65f536f260a92be
Eka666Sed <cat.sedoff@gmail.com>
2024-07-26 22:52:49 +0200

    140_добавляет подключение Api для жанров

--- app/src/main/java/com/davay/android/feature/createsession/presentation/genre/GenreFragment.kt
+++ app/src/main/java/com/davay/android/feature/createsession/presentation/genre/GenreFragment.kt
@@ -2,18 +2,22 @@ package com.davay.android.feature.createsession.presentation.genre
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import com.davay.android.base.BaseFragment
 import com.davay.android.databinding.FragmentGenreBinding
 import com.davay.android.di.AppComponentHolder
 import com.davay.android.di.ScreenComponent
 import com.davay.android.feature.createsession.di.DaggerCreateSessionFragmentComponent
-import com.davay.android.feature.createsession.domain.model.GenreSelect
 import com.davay.android.feature.createsession.presentation.genre.adapter.GenreAdapter
+import com.davay.android.utils.presentation.UiErrorHandler
+import com.davay.android.utils.presentation.UiErrorHandlerImpl
 import com.google.android.flexbox.AlignItems
 import com.google.android.flexbox.FlexDirection
 import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayoutManager
 import com.google.android.flexbox.JustifyContent
+import kotlinx.coroutines.launch
 
 class GenreFragment : BaseFragment<FragmentGenreBinding, GenreViewModel>(
     FragmentGenreBinding::inflate
@@ -23,6 +27,8 @@ class GenreFragment : BaseFragment<FragmentGenreBinding, GenreViewModel>(
         viewModel.genreClicked(genre)
     }
 
+    private val errorHandler: UiErrorHandler = UiErrorHandlerImpl()
+
     override fun diComponent(): ScreenComponent = DaggerCreateSessionFragmentComponent.builder()
         .appComponent(AppComponentHolder.getComponent())
         .build()
@@ -30,6 +36,15 @@ class GenreFragment : BaseFragment<FragmentGenreBinding, GenreViewModel>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRecycler()
+    }
+
+    override fun subscribe() {
+        super.subscribe()
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.state.collect { state ->
+                renderState(state)
+            }
+        }
     }
 
     private fun initRecycler() {
@@ -41,21 +56,62 @@ class GenreFragment : BaseFragment<FragmentGenreBinding, GenreViewModel>(
             alignItems = AlignItems.FLEX_START
         }
         binding.rvGenre.layoutManager = layoutManager
+
         // временно для теста
-        genreAdapter.addItemList(
-            listOf(
-                GenreSelect(1, "Ужасы"),
-                GenreSelect(1, "Комедия"),
-                GenreSelect(1, "Боевик"),
-                GenreSelect(1, "Ужасы2"),
-                GenreSelect(1, "Ужасы3"),
-                GenreSelect(1, "Боевик2"),
-                GenreSelect(1, "Ужасы4"),
-                GenreSelect(1, "Комедия2")
-            )
-        )
+//        genreAdapter.addItemList(
+//            listOf(
+//                GenreSelect(1, "Ужасы"),
+//                GenreSelect(1, "Комедия"),
+//                GenreSelect(1, "Боевик"),
+//                GenreSelect(1, "Ужасы2"),
+//                GenreSelect(1, "Ужасы3"),
+//                GenreSelect(1, "Боевик2"),
+//                GenreSelect(1, "Ужасы4"),
+//                GenreSelect(1, "Комедия2")
+//            )
+//        )
     }
 
+    private fun renderState(state: GenreState) {
+        when (state) {
+            is GenreState.Loading -> showProgressBar()
+            is GenreState.Content -> handleContent(state)
+            is GenreState.Error -> handleError(state)
+        }
+    }
+
+    private fun handleError(state: GenreState.Error) {
+        showErrorMessage()
+        errorHandler.handleError(
+            state.errorType,
+            binding.errorMessage
+        ) {
+            viewModel.getGenreList()
+        }
+    }
+
+    private fun showErrorMessage() = with(binding) {
+        errorMessage.isVisible = true
+        progressBar.isVisible = false
+        rvGenre.isVisible = false
+    }
+
+    private fun handleContent(state: GenreState.Content) {
+        genreAdapter.addItemList(state.genreList)
+        showContent()
+    }
+
+    private fun showContent() = with(binding) {
+        errorMessage.isVisible = false
+        progressBar.isVisible = false
+        rvGenre.isVisible = true
+    }
+
+    private fun showProgressBar() = with(binding) {
+        errorMessage.isVisible = false
+        progressBar.isVisible = true
+        rvGenre.isVisible = false
+    }
     companion object {
         fun newInstance() = GenreFragment()
     }
--- app/src/main/java/com/davay/android/feature/createsession/presentation/genre/GenreViewModel.kt
+++ app/src/main/java/com/davay/android/feature/createsession/presentation/genre/GenreViewModel.kt
@@ -1,23 +1,108 @@
 package com.davay.android.feature.createsession.presentation.genre
 
 import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.commit451.translationviewdraghelper.BuildConfig
 import com.davay.android.base.BaseViewModel
+import com.davay.android.core.domain.models.ErrorScreenState
+import com.davay.android.core.domain.models.ErrorType
+import com.davay.android.core.domain.models.Genre
+import com.davay.android.core.domain.models.Result
 import com.davay.android.feature.createsession.domain.model.GenreSelect
+import com.davay.android.feature.createsession.domain.usecase.GetGenresUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class GenreViewModel @Inject constructor() : BaseViewModel() {
+class GenreViewModel @Inject constructor(
+    private val getGenresUseCase: GetGenresUseCase
+) : BaseViewModel() {
+    private val _state = MutableStateFlow<GenreState>(GenreState.Loading)
+    val state = _state.asStateFlow()
 
-    private val selectedGenreList = mutableListOf<GenreSelect>()
+    private val selectedGenre = mutableListOf<GenreSelect>()
+
+    init {
+        getGenreList()
+    }
+
+   private fun getGenreList() {
+        _state.update { GenreState.Loading }
+       runSafelyUseCase(
+            useCaseFlow = getGenresUseCase.execute(),
+            onSuccess = { genres ->
+                if (genres.isEmpty()) {
+                    _state.update { GenreState.Error(ErrorScreenState.EMPTY) }
+                } else {
+                    val genre = genres.map { it.toGenre() }
+                    _state.update { GenreState.Content(genre) }
+                }
+            },
+            onFailure = { error ->
+                _state.update { GenreState.Error(renderError(error)) }
+            }
+        )
+    }
+
+    private fun renderError(errorType: ErrorType): ErrorScreenState {
+        return when (errorType) {
+            ErrorType.NO_CONNECTION -> ErrorScreenState.NO_INTERNET
+            ErrorType.NOT_FOUND -> ErrorScreenState.SERVER_ERROR
+            ErrorType.BAD_REQUEST -> ErrorScreenState.SERVER_ERROR
+            ErrorType.APP_VERSION_ERROR -> ErrorScreenState.APP_VERSION_ERROR
+            else -> ErrorScreenState.SERVER_ERROR
+        }
+    }
 
     fun genreClicked(genre: GenreSelect) {
         if (genre.isSelected) {
-            selectedGenreList.add(genre)
+            selectedGenre.add(genre)
         } else {
-            selectedGenreList.remove(genre)
+            selectedGenre.remove(genre)
         }
     }
 
     fun buttonContinueClicked() {
-        Log.d("MyTag", selectedGenreList.toString())
+        Log.d("MyTag", selectedGenre.toString())
+    }
+
+    private inline fun <reified D> runSafelyUseCase(
+        useCaseFlow: Flow<Result<D, ErrorType>>,
+        noinline onFailure: ((ErrorType) -> Unit)? = null,
+        crossinline onSuccess: (D) -> Unit,
+    ) {
+        viewModelScope.launch(Dispatchers.IO) {
+            runCatching {
+                useCaseFlow.collect { result ->
+                    when (result) {
+                        is Result.Success -> onSuccess(result.data)
+                        is Result.Error -> {
+                            onFailure?.invoke(result.error)
+                                ?: _state.update { GenreState.Error(renderError(result.error)) }
+                        }
+                    }
+                }
+            }.onFailure { error ->
+                if (BuildConfig.DEBUG) {
+                    Log.v(TAG, "error -> ${error.localizedMessage}")
+                    error.printStackTrace()
+                }
+                onFailure?.invoke(ErrorType.UNKNOWN_ERROR)
+                    ?: _state.update { GenreState.Error(renderError(ErrorType.UNKNOWN_ERROR)) }
+            }
+        }
+    }
+
+    private fun Genre.toGenre() = GenreSelect(
+        name = this.name,
+       isSelected = false
+        )
+
+    private companion object {
+        val TAG = GenreViewModel::class.simpleName
     }
 }
