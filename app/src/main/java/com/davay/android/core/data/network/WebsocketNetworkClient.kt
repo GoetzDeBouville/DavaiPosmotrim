bc56cd17ae2088d7d168d03c7392c4b27d96185f
Dmitry Kryushnikov <civ02@mail.ru>
2024-07-28 22:35:18 +0500

    141_интерфейс, абстрактный класс вебсокета

---
+++ app/src/main/java/com/davay/android/core/data/network/WebsocketNetworkClient.kt
@@ -0,0 +1,9 @@
+package com.davay.android.core.data.network
+
+import kotlinx.coroutines.flow.Flow
+
+interface WebsocketNetworkClient<O, M> {
+    fun subscribe(): Flow<O>
+    suspend fun sendMessage(message: M)
+    suspend fun close()
+}
