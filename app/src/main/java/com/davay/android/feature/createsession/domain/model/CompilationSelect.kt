bae76d3bb57fe8b34dd74b01c7b4aac55ea2e80a
Eka666Sed <cat.sedoff@gmail.com>
2024-07-29 14:35:34 +0200

    140_убирает аннотацию @Suppress("DataClassShouldBeImmutable")

--- app/src/main/java/com/davay/android/feature/createsession/domain/model/CompilationSelect.kt
+++ app/src/main/java/com/davay/android/feature/createsession/domain/model/CompilationSelect.kt
@@ -4,6 +4,5 @@ data class CompilationSelect(
     val id: String,
     val name: String,
     val cover: String,
-    @Suppress("DataClassShouldBeImmutable")
     val isSelected: Boolean = false,
 )
--- app/src/main/java/com/davay/android/feature/createsession/domain/model/GenreSelect.kt
+++ app/src/main/java/com/davay/android/feature/createsession/domain/model/GenreSelect.kt
@@ -2,6 +2,5 @@ package com.davay.android.feature.createsession.domain.model
 
 data class GenreSelect(
     val name: String,
-    @Suppress("DataClassShouldBeImmutable")
     val isSelected: Boolean = false,
 )
