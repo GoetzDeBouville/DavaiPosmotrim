28e9a153674ff9e3356263f3d97b829af0248c0b
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-08-09 17:57:12 +0300

    142_добавляет в HttpCreateSessionKtorClient post запрос для создания сессии провайдит в клиента userDataRepository для получения данынх пользователя с последующим использованием в header

--- app/src/main/java/com/davay/android/feature/createsession/data/network/HttpCreateSessionKtorClient.kt
+++ app/src/main/java/com/davay/android/feature/createsession/data/network/HttpCreateSessionKtorClient.kt
@@ -2,23 +2,54 @@ package com.davay.android.feature.createsession.data.network
 
 import android.content.Context
 import com.davay.android.core.data.network.HttpKtorNetworkClient
+import com.davay.android.core.domain.api.UserDataRepository
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.headers
 import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.http.path
 import javax.inject.Inject
 
 class HttpCreateSessionKtorClient @Inject constructor(
     context: Context,
-    private val httpClient: HttpClient
+    private val httpClient: HttpClient,
+    private val userDataRepository: UserDataRepository
 ) : HttpKtorNetworkClient<CreateSessionRequest, CreateSessionResponse>(context) {
     override suspend fun sendResponseByType(request: CreateSessionRequest): HttpResponse {
-        return httpClient.get {
-            url {
-                path(request.path)
-                parameter("format", "json")
+        return when(request) {
+            is CreateSessionRequest.Session -> {
+                httpClient.post {
+                    url {
+                        path(request.path)
+                        parameter("format", "json")
+                    }
+
+                    headers {
+                        append(DEVICE_ID, userDataRepository.getUserId())
+                    }
+
+                    contentType(ContentType.Application.Json)
+                    setBody(mapOf(request.parameter to request.requestBody))
+                }
+            }
+
+            else -> {
+                httpClient.get {
+                    url {
+                        path(request.path)
+                        parameter("format", "json")
+                    }
+
+                    headers {
+                        append(DEVICE_ID, userDataRepository.getUserId())
+                    }
+                }
             }
         }
     }
@@ -35,6 +66,14 @@ class HttpCreateSessionKtorClient @Inject constructor(
             is CreateSessionRequest.GenreList -> {
                 CreateSessionResponse.GenreList(httpResponse.body())
             }
+
+            is CreateSessionRequest.Session -> {
+                CreateSessionResponse.Session(httpResponse.body())
+            }
         }
+    }
+
+    private companion object {
+        const val DEVICE_ID = "device-id"
     }
 }
