8f1559873fa7881d0347801d54ab8d3a07e79667
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-08-09 17:17:58 +0300

    142_добавляет новые стэйты для экранаколлекций

--- app/src/main/java/com/davay/android/feature/createsession/presentation/compilations/CompilationsState.kt
+++ app/src/main/java/com/davay/android/feature/createsession/presentation/compilations/CompilationsState.kt
@@ -1,10 +1,13 @@
 package com.davay.android.feature.createsession.presentation.compilations
 
 import com.davay.android.core.domain.models.ErrorScreenState
+import com.davay.android.core.domain.models.Session
 import com.davay.android.feature.createsession.domain.model.CompilationSelect
 
 sealed class CompilationsState {
     data object Loading : CompilationsState()
     class Error(val errorType: ErrorScreenState) : CompilationsState()
     class Content(val compilationList: List<CompilationSelect>) : CompilationsState()
+    data object CreateSessionLoading : CompilationsState()
+    class SessionCreated(val session: Session) : CompilationsState()
 }
