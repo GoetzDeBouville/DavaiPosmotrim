992a30a2aad6b8876e24a00057802b4aefe22eb4
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-08-09 17:29:03 +0300

    142_добавляет стэйт в GenreState

--- app/src/main/java/com/davay/android/feature/createsession/presentation/genre/GenreState.kt
+++ app/src/main/java/com/davay/android/feature/createsession/presentation/genre/GenreState.kt
@@ -1,10 +1,14 @@
 package com.davay.android.feature.createsession.presentation.genre
 
 import com.davay.android.core.domain.models.ErrorScreenState
+import com.davay.android.core.domain.models.Session
 import com.davay.android.feature.createsession.domain.model.GenreSelect
+import com.davay.android.feature.createsession.presentation.compilations.CompilationsState
 
 sealed class GenreState {
     data object Loading : GenreState()
     class Error(val errorType: ErrorScreenState) : GenreState()
     class Content(val genreList: List<GenreSelect>) : GenreState()
+    data object CreateSessionLoading : GenreState()
+    class SessionCreated(val session: Session) : GenreState()
 }
