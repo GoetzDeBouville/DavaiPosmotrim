99944b7a917a25fce034f4892cc9db798eb86442
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-08-14 02:11:50 +0300

    149_добавляет стэйты ошибок для обработки случаев неудачного создания сессии

--- app/src/main/java/com/davay/android/core/domain/models/ErrorScreenState.kt
+++ app/src/main/java/com/davay/android/core/domain/models/ErrorScreenState.kt
@@ -18,6 +18,18 @@ enum class ErrorScreenState(
         com.davai.uikit.R.drawable.ic_error_no_service_access_200,
         null
     ),
+    ERROR_BUILD_SESSEION_GENRES(
+        com.davai.uikit.R.string.error_message_no_build_service_error,
+        com.davai.uikit.R.string.error_message_no_build_service_error_description_genres,
+        com.davai.uikit.R.drawable.ic_error_no_service_access_200,
+        com.davai.uikit.R.string.error_message_no_build_service_error_button_text_genres,
+    ),
+    ERROR_BUILD_SESSEION_COLLECTIONS(
+        com.davai.uikit.R.string.error_message_no_build_service_error,
+        com.davai.uikit.R.string.error_message_no_build_service_error_description_collections,
+        com.davai.uikit.R.drawable.ic_error_no_service_access_200,
+        com.davai.uikit.R.string.error_message_no_build_service_error_button_text_collections,
+    ),
     EMPTY(
         com.davai.uikit.R.string.error_message_empty_title,
         com.davai.uikit.R.string.empty_text,
