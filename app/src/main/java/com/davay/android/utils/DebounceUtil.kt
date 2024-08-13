97fed7ad4133d0e917cc0c29f236497bb6232a98
Eka666Sed <cat.sedoff@gmail.com>
2024-08-13 19:54:40 +0200

    128_добавляет функцию setDebouncedNavigation для навигации

--- app/src/main/java/com/davay/android/utils/DebounceUtil.kt
+++ app/src/main/java/com/davay/android/utils/DebounceUtil.kt
@@ -1,9 +1,13 @@
 package com.davay.android.utils
 
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 
 object DebounceUtil {
     private const val DEFAULT_DEBOUNCE_TIME = 600L
+    private val handler = Handler(Looper.getMainLooper())
+    private var lastNavigationTime: Long = 0
 
     fun View.setDebouncedOnClickListener(
         debounceTime: Long = DEFAULT_DEBOUNCE_TIME,
@@ -19,6 +23,17 @@ object DebounceUtil {
                 action()
             }
             handler.postDelayed(runnable!!, debounceTime)
+        }
+    }
+
+    fun setDebouncedNavigation(
+        debounceTime: Long = DEFAULT_DEBOUNCE_TIME,
+        action: () -> Unit
+    ) {
+        val currentTime = System.currentTimeMillis()
+        if (currentTime - lastNavigationTime >= debounceTime) {
+            lastNavigationTime = currentTime
+            action()
         }
     }
 }
