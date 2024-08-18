c048ce9b30c5467e705c9c9edfda299cfa88284b
Dmitry Kryushnikov <civ02@mail.ru>
2024-08-18 21:44:53 +0500

    134_отступ на SessionListFragment

--- app/src/main/java/com/davay/android/feature/sessionlist/presentation/SessionListFragment.kt
+++ app/src/main/java/com/davay/android/feature/sessionlist/presentation/SessionListFragment.kt
@@ -10,6 +10,7 @@ import com.davay.android.base.BaseFragment
 import com.davay.android.databinding.FragmentSessionListBinding
 import com.davay.android.di.AppComponentHolder
 import com.davay.android.di.ScreenComponent
+import com.davay.android.extensions.setBottomMargin
 import com.davay.android.feature.sessionlist.di.DaggerSessionListFragmentComponent
 import com.davay.android.feature.sessionlist.presentation.adapter.CustomItemDecorator
 import com.davay.android.feature.sessionlist.presentation.adapter.UserAdapter
@@ -48,7 +49,7 @@ class SessionListFragment : BaseFragment<FragmentSessionListBinding, SessionList
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        setBottomMargin(binding.btnExit)
         dialog = MainDialogFragment.newInstance(
             title = getString(R.string.leave_session_title),
             message = getString(R.string.leave_session_dialog_message),
