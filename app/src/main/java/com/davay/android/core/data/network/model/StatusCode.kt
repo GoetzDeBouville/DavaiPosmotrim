d7853808219c7e6d9854b0056815a76a4f458889
muskilon <max-post@yandex.ru>
2024-08-20 00:12:37 +0300

    147_Вернул StatusCode как было

--- app/src/main/java/com/davay/android/core/data/network/model/StatusCode.kt
+++ app/src/main/java/com/davay/android/core/data/network/model/StatusCode.kt
@@ -2,15 +2,15 @@ package com.davay.android.core.data.network.model
 
 import com.davay.android.core.domain.models.ErrorType
 
-class StatusCode(private val code: Int) {
+@JvmInline
+value class StatusCode(val code: Int)
 
-    fun mapToErrorType(): ErrorType {
-        return when (code) {
-            NetworkParams.NO_CONNECTION_CODE -> ErrorType.NO_CONNECTION
-            NetworkParams.BAD_REQUEST_CODE -> ErrorType.BAD_REQUEST
-            NetworkParams.NOT_FOUND_CODE -> ErrorType.NOT_FOUND
-            NetworkParams.SERVER_ERROR_CODE -> ErrorType.SERVER_ERROR
-            else -> ErrorType.UNKNOWN_ERROR
-        }
+fun StatusCode.mapToErrorType(): ErrorType {
+    return when (code) {
+        NetworkParams.NO_CONNECTION_CODE -> ErrorType.NO_CONNECTION
+        NetworkParams.BAD_REQUEST_CODE -> ErrorType.BAD_REQUEST
+        NetworkParams.NOT_FOUND_CODE -> ErrorType.NOT_FOUND
+        NetworkParams.SERVER_ERROR_CODE -> ErrorType.SERVER_ERROR
+        else -> ErrorType.UNKNOWN_ERROR
     }
 }
