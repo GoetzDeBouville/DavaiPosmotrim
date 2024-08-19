e420aede003329c47b1945755806d155e708fd57
Eka666Sed <cat.sedoff@gmail.com>
2024-08-19 15:58:22 +0200

    154_подключает плагин safeArgs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.serialization)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.navigationSafeArgs)
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.detekt)
+    alias(libs.plugins.navigationSafeArgs) apply false
 }
 
 
