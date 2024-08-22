d45cb9a99be059a7854f59bca47f29fe292339d8
Dmitry Kryushnikov <civ02@mail.ru>
2024-08-22 23:24:39 +0500

    134_убрал условие для нижнего отступа

--- app/src/main/java/com/davay/android/extensions/FragmentBottomMargin.kt
+++ app/src/main/java/com/davay/android/extensions/FragmentBottomMargin.kt
@@ -7,23 +7,18 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
 
-const val DENOMINATOR_TWO = 2
-
 fun Fragment.setBottomMargin(view: View) {
     ViewCompat.setOnApplyWindowInsetsListener(view) { v, windowInsets ->
         val insetNavigationBar =
             windowInsets.getInsets(WindowInsetsCompat.Type.navigationBars()).bottom
-        if (v.height / DENOMINATOR_TWO <= insetNavigationBar && v.height > 0) {
-            if (windowInsets.isVisible(WindowInsetsCompat.Type.ime())) {
-                v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                    bottomMargin = 0
-                }
-            } else {
-                v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                    bottomMargin = insetNavigationBar
-                }
+        if (windowInsets.isVisible(WindowInsetsCompat.Type.ime())) {
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                bottomMargin = 0
             }
-
+        } else {
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                bottomMargin = insetNavigationBar
+            }
         }
         windowInsets
     }
