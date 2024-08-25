4f8fcc03197a89a2597680d56cd6f0fb2831ec31
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-08-25 14:57:32 +0300

    149_добавляет sealed интерфейс для стэйта

---
+++ app/src/main/java/com/davay/android/feature/selectmovie/presentation/SelectMovieState.kt
@@ -0,0 +1,10 @@
+package com.davay.android.feature.selectmovie.presentation
+
+import com.davay.android.core.domain.models.ErrorScreenState
+import com.davay.android.core.domain.models.MovieDetails
+
+sealed interface SelectMovieState {
+    data object Loading : SelectMovieState
+    class Error(val errorType: ErrorScreenState) : SelectMovieState
+    class Content(val movieList: List<MovieDetails>) : SelectMovieState
+}
