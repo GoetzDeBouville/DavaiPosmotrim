d0c2a06aae72e31a0e357c02572d8c9aa48520bd
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-08-28 23:04:01 +0300

    149_добавляет поле isLiked в MovieIdEntity

--- app/src/main/java/com/davay/android/core/data/database/entity/MovieIdEntity.kt
+++ app/src/main/java/com/davay/android/core/data/database/entity/MovieIdEntity.kt
@@ -7,5 +7,6 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "movie_ids")
 data class MovieIdEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    @ColumnInfo(name = "movie_id") val movieId: Int
+    @ColumnInfo(name = "movie_id") val movieId: Int,
+    @ColumnInfo(name = "is_liked") val isLiked: Boolean = false
 )
