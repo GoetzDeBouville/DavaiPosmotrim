66eeb6155572c762a5d67bf2e451c41e9016adc6
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-09-09 20:51:43 +0300

    142_меняет стэйты с класса на интерфейсы

--- app/src/main/java/com/davay/android/feature/createsession/presentation/genre/GenreState.kt
+++ app/src/main/java/com/davay/android/feature/createsession/presentation/genre/GenreState.kt
@@ -1,12 +1,11 @@
 package com.davay.android.feature.createsession.presentation.genre
 
 import com.davay.android.core.domain.models.ErrorScreenState
-import com.davay.android.core.domain.models.Session
 import com.davay.android.feature.createsession.domain.model.GenreSelect
 
-sealed class GenreState {
-    data object Loading : GenreState()
-    class Error(val errorType: ErrorScreenState) : GenreState()
-    class Content(val genreList: List<GenreSelect>) : GenreState()
-    data object CreateSessionLoading : GenreState()
+sealed interface GenreState {
+    data object Loading : GenreState
+    class Error(val errorType: ErrorScreenState) : GenreState
+    class Content(val genreList: List<GenreSelect>) : GenreState
+    data object CreateSessionLoading : GenreState
 }
