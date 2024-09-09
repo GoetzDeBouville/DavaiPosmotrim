578f80113102902e4196ad1da8282c3f38b111fc
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-09-09 20:55:55 +0300

    142_удаляет не используемые интерфейсы

--- app/src/main/java/com/davay/android/feature/waitsession/data/network/WaitSessionRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.davay.android.feature.waitsession.data.network
-
-sealed interface WaitSessionRequest {
-    val path: String
-
-    data object Session : WaitSessionRequest {
-        override val path: String = "api/collections/"
-    }
-}
--- app/src/main/java/com/davay/android/feature/waitsession/data/network/WaitSessionResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.davay.android.feature.waitsession.data.network
-
-import com.davay.android.core.data.dto.CollectionDto
-
-sealed interface WaitSessionResponse {
-    class CollectionList(val value: List<CollectionDto>) : WaitSessionResponse
-}
