b608c51f8a6d86e2946797d0eb148bc76a633d0c
Dmitry Kryushnikov <civ02@mail.ru>
2024-09-28 14:28:42 +0500

    165_класс HttpConnectToSessionKtorClient

--- app/src/main/java/com/davay/android/feature/sessionlist/data/network/ConnectToSessionRequest.kt
+++ app/src/main/java/com/davay/android/feature/sessionlist/data/network/ConnectToSessionRequest.kt
@@ -1,9 +1,9 @@
 package com.davay.android.feature.sessionlist.data.network
 
 sealed class ConnectToSessionRequest(val path: String) {
-    class ConnectToSession(val sessionId: String) :
+    class ConnectToSession(val sessionId: String, val userId: String) :
         ConnectToSessionRequest(path = "api/sessions/$sessionId/connection/")
 
-    class Session(val sessionId: String) :
+    class Session(val sessionId: String, val userId: String) :
         ConnectToSessionRequest(path = "api/sessions/$sessionId/")
 }
