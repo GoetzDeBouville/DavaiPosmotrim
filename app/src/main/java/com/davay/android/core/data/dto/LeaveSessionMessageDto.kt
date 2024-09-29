811a43cc0d6bf15801ee8f1ac477bd08f378647e
Dmitry Kryushnikov <civ02@mail.ru>
2024-09-29 20:42:28 +0500

    165_перенос файла в другой пакет

--- app/src/main/java/com/davay/android/core/data/dto/LeaveSessionMessageDto.kt
+++ app/src/main/java/com/davay/android/core/data/dto/LeaveSessionMessageDto.kt
@@ -1,4 +1,4 @@
-package com.davay.android.core.data.network.model
+package com.davay.android.core.data.dto
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
--- app/src/main/java/com/davay/android/core/data/network/model/LeaveSessionResponse.kt
+++ app/src/main/java/com/davay/android/core/data/network/model/LeaveSessionResponse.kt
@@ -1,5 +1,7 @@
 package com.davay.android.core.data.network.model
 
+import com.davay.android.core.data.dto.LeaveSessionMessageDto
+
 class LeaveSessionResponse(
     val value: LeaveSessionMessageDto
 )
