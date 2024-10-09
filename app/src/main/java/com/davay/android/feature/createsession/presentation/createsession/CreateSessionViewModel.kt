0ba19e44ae799013125f105fc8edc7b6f9b89a6f
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-10-09 23:42:45 +0300

    167_добавляет методы подключения вебсокетов

--- app/src/main/java/com/davay/android/feature/createsession/presentation/createsession/CreateSessionViewModel.kt
+++ app/src/main/java/com/davay/android/feature/createsession/presentation/createsession/CreateSessionViewModel.kt
@@ -3,12 +3,64 @@ package com.davay.android.feature.createsession.presentation.createsession
 import android.os.Bundle
 import com.davay.android.R
 import com.davay.android.base.BaseViewModel
+import com.davay.android.core.domain.impl.CommonWebsocketInteractor
 import com.davay.android.core.domain.models.SessionShort
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import javax.inject.Inject
 
-open class CreateSessionViewModel @Inject constructor() : BaseViewModel() {
+open class CreateSessionViewModel @Inject constructor(
+    private val commonWebsocketInteractor: CommonWebsocketInteractor,
+) : BaseViewModel() {
+
+    protected fun subscribeToWebsocketsAndUpdateSessionId(sessionId: String) {
+        commonWebsocketInteractor.updateSessionId(sessionId)
+        subscribeToUsers()
+        subscribeSessionStatus()
+        subscribeSessionResult()
+        subscribeMatchesId()
+        subscribeRouletteId()
+    }
+
+    private fun subscribeToUsers() {
+        runSafelyUseCaseWithNullResponse(
+            useCaseFlow = commonWebsocketInteractor.subscribeUsers(),
+            onSuccess = {},
+            onFailure = {}
+        )
+    }
+
+    private fun subscribeSessionStatus() {
+        runSafelyUseCaseWithNullResponse(
+            useCaseFlow = commonWebsocketInteractor.subscribeSessionStatus(),
+            onSuccess = {},
+            onFailure = {}
+        )
+    }
+
+    private fun subscribeSessionResult() {
+        runSafelyUseCaseWithNullResponse(
+            useCaseFlow = commonWebsocketInteractor.subscribeSessionResult(),
+            onSuccess = {},
+            onFailure = {}
+        )
+    }
+
+    private fun subscribeMatchesId() {
+        runSafelyUseCaseWithNullResponse(
+            useCaseFlow = commonWebsocketInteractor.subscribeMatchesId(),
+            onSuccess = {},
+            onFailure = {}
+        )
+    }
+
+    private fun subscribeRouletteId() {
+        runSafelyUseCaseWithNullResponse(
+            useCaseFlow = commonWebsocketInteractor.subscribeRouletteId(),
+            onSuccess = {},
+            onFailure = {}
+        )
+    }
 
     protected fun navigateToWaitSession(session: SessionShort) {
         val sessionJson = Json.encodeToString(session)
