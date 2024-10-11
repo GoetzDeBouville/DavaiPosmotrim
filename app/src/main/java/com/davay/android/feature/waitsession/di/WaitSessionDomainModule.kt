f8c7b59b003aab9acc712baf6f3dec1e71b51258
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-10-12 01:52:51 +0300

    167_провайдит зависимости

--- app/src/main/java/com/davay/android/feature/waitsession/di/WaitSessionDomainModule.kt
+++ app/src/main/java/com/davay/android/feature/waitsession/di/WaitSessionDomainModule.kt
@@ -1,10 +1,13 @@
 package com.davay.android.feature.waitsession.di
 
 import com.davay.android.core.domain.api.UserDataRepository
+import com.davay.android.core.domain.impl.CommonWebsocketInteractor
 import com.davay.android.core.domain.impl.GetUserDataUseCaseImpl
 import com.davay.android.core.domain.lounchcontrol.api.FirstTimeFlagRepository
 import com.davay.android.core.domain.usecases.GetUserDataUseCase
+import com.davay.android.feature.waitsession.domain.SetSessionStatusVotingUseCase
 import com.davay.android.feature.waitsession.domain.WaitSessionOnBoardingInteractorImpl
+import com.davay.android.feature.waitsession.domain.api.StartVotingSessionStatusRepository
 import com.davay.android.feature.waitsession.domain.api.WaitSessionOnBoardingInteractor
 import dagger.Module
 import dagger.Provides
@@ -20,4 +23,11 @@ class WaitSessionDomainModule {
     fun provideGetUserIdUseCase(
         repository: UserDataRepository
     ): GetUserDataUseCase = GetUserDataUseCaseImpl(repository)
+
+    @Provides
+    fun provideSetSessionStatusVotingUseCase(
+        commonWebsocketInteractor: CommonWebsocketInteractor,
+        repository: StartVotingSessionStatusRepository
+    ) = SetSessionStatusVotingUseCase(commonWebsocketInteractor, repository)
+
 }
